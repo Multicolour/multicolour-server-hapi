7b9d84c90c3bba828d9ca8a3d09d4539f8c14941
davemackintosh <me@davemackintosh.co.uk>
2016-02-25 19:31:42 +0000

    Tests.
    Signed-off-by: davemackintosh <me@davemackintosh.co.uk>

--- tests/all.js
+++ tests/all.js
@@ -14,11 +14,6 @@ const Multicolour_Hapi_Server = require("../index")
 // Where we keep the test content.
 const test_content_path = "./tests/test_content"
 
-const inject_options = {
-  url: "/test",
-  headers: { accept: "application/json" }
-}
-
 // Create a fake plugin for the server.
 class Test_Plugin extends Map {
   constructor() { super() }
@@ -39,57 +34,34 @@ class Test_Plugin extends Map {
 }
 
 // Create a multicolour instance.
-let multicolour = Multicolour
+const multicolour = Multicolour
   .new_from_config_file_path(`${test_content_path}/config.js`)
   .scan()
+
+let server
 
 // Start the database.
 multicolour.get("database").start(() => {
   tape("Plugin compatibility.", test => {
     test.doesNotThrow(() => multicolour.use(Multicolour_Hapi_Server), "Multicolour Hapi Server is registered as a plugin without error.")
+    server = multicolour.get("server")
     test.end()
   })
 
   tape("Plugin predictability/coverage.", test => {
+    test.plan(4)
     // Test for items we expect to exist/function once register.
     test.ok(multicolour.get("server"), "Multicolour Hapi Server is configured as plugin correctly and can `get`.")
     test.ok(multicolour.get("server").request("raw"), "Can get raw server from plugin")
     test.doesNotThrow(() => multicolour.get("server").use(Test_Plugin), "Can register plugins without error")
-
-    test.end()
-  })
-
-  tape("Default decorator and routes are functional.", test => {
-    multicolour.get("server").generate_routes()
-
-    // Run a fake request.
-    multicolour
-      .get("server")
-        .request("raw")
-          .inject(inject_options, response => {
-            test.equal(response.statusCode, 200, "Response code should be 200")
-          })
+    test.doesNotThrow(() => server.start(server.stop.bind(server)), "Server starts and stops without error.")
   })
 
   tape("Header validator can set and get headers.", test => {
-    // Run a fake request.
-    const headers = multicolour.get("server").request("header_validator")
+    const headers = server.request("header_validator")
 
     test.doesNotThrow(() => headers.set("test", 123), "Can set a test header")
     test.equal(headers.get("test"), 123, "Getting test header value")
     test.end()
-  })
-
-  tape("Starts and stops without error", test => {
-    const server = multicolour.get("server")
-    test.plan(1)
-
-    multicolour = Multicolour
-      .new_from_config_file_path(`${test_content_path}/config.js`)
-      .scan()
-      .use(Multicolour_Hapi_Server)
-
-    // This is just for coverage.
-    test.doesNotThrow(() => server.start(server.stop.bind(server)), "Starts and stops without error.")
   })
 })
---
+++ tests/test_content/blueprints/weak.js
@@ -0,0 +1,10 @@
+"use strict"
+
+module.exports = {
+  attributes: {
+    name: {
+      required: true,
+      type: "string"
+    }
+  }
+}
