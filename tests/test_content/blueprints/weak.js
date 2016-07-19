b3baf3f10c5c62c679384fac66abc4110c515723
davemackintosh <me@davemackintosh.co.uk>
2016-07-19 09:07:06 +0100

    Coverage :: Convert simple model to Endpoint for test coverage.
    Signed-off-by: davemackintosh <me@davemackintosh.co.uk>

--- tests/test_content/blueprints/weak.js
+++ tests/test_content/blueprints/weak.js
@@ -1,10 +1,15 @@
 "use strict"
 
-module.exports = {
-  attributes: {
-    name: {
-      required: true,
-      type: "string"
-    }
+const Endpoint = require("multicolour/endpoint")
+
+module.exports = new Endpoint({
+  name: {
+    required: true,
+    type: "string"
   }
-}
+})
+.add_create_route()
+.add_read_route()
+.add_update_route()
+.add_delete_route()
+.add_update_or_create_route()
